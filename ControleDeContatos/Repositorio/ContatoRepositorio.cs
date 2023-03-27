b0d90af874802ae68533ed2df5b38cc318d22d48
Felipe Antunes de Oliveira <felipe.tunes@outlook.com>
2023-03-27 00:50:35 -0300

    Add project files.

---
+++ ControleDeContatos/Repositorio/ContatoRepositorio.cs
@@ -0,0 +1,48 @@
+﻿using ControleDeContatos.Data;
+using ControleDeContatos.Models;
+
+namespace ControleDeContatos.Repositorio
+{
+    public class ContatoRepositorio : IContatoRepositorio
+    {
+        private readonly BancoContext _context;
+        public ContatoRepositorio(BancoContext bancoContext)
+        {
+            _context = bancoContext;
+        }
+
+        public ContatoModel Adicionar(ContatoModel contato)
+        {
+            _context.Contatos.Add(contato);
+            _context.SaveChanges();
+            
+            return contato;
+        }
+
+        public ContatoModel Atualizar(ContatoModel contato)
+        {
+            ContatoModel contatoDB = ListarPorId(contato.Id);
+
+            if(contatoDB == null) throw new System.Exception("Houve um erro na atualização do contato");
+
+            contatoDB.Nome = contato.Nome;
+            contatoDB.Email = contato.Email;
+            contatoDB.Celular = contato.Celular;
+
+            _context.Contatos.Update(contatoDB);
+            _context.SaveChanges();
+
+            return contatoDB;
+        }
+
+        public List<ContatoModel> BuscarTodos()
+        {
+            return _context.Contatos.ToList();
+        }
+
+        public ContatoModel ListarPorId(int id)
+        {
+            return _context.Contatos.FirstOrDefault(x => x.Id == id);
+        }
+    }
+}
